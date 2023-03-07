b091bdf699c44abe6e7e79a3010488215f687998
Mkupr <mkupr@outlook.com>
2023-03-07 20:49:52 +0300

    add filter_by_state to utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 
 def get_operations():
@@ -7,5 +8,12 @@ def get_operations():
         for item in json.loads("".join(data_file.readlines())):
             if item:
                 result.append(item)
+    return result
+
+def filter_by_state(data_list: list, st_val: str) -> list:
+    result = []
+    for item_dict in data_list:
+        if item_dict['state'] == 'EXECUTED':
+            result.append(item_dict)
     return result
 
